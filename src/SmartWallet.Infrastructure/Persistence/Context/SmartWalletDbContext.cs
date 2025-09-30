2a30cd90cb51c744d9100c509c083030b6758ac1
Malcom Foxa <malcom.foca@gmail.com>
2025-09-30 20:10:22 -0300

    feat: agregar SmartWalletDbContext y registrarlo en Program.cs con validación desde .env

---
+++ src/SmartWallet.Infrastructure/Persistence/Context/SmartWalletDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartWallet.Domain.Entities;
+
+
+namespace SmartWallet.Infrastructure.Persistence.Context
+{
+    public class SmartWalletDbContext : DbContext
+    {
+        // agregar los DbSet para tus entidades
+
+        public SmartWalletDbContext(DbContextOptions<SmartWalletDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // agregar relaciones entre entidades si es necesario
+        }
+    }
+}
