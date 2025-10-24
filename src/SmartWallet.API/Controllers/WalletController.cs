49d01bf9b7c415cdc21d4cfcd6d5a374c8e59f3e
Ezequiel Natale <40499263+Eze-Nat@users.noreply.github.com>
2025-10-24 18:45:53 -0300

    Refactor/wallet entity (#24)
    * creando entidad wallet
    
    * Creado IWalletService, Corregido WalletController
    
    * corregido codigo
    
    * agregada documentacion, modificacion de controllers, response y request de wallet, swagger funcionando

---
+++ src/SmartWallet.API/Controllers/WalletController.cs
@@ -0,0 +1,64 @@
+﻿using Contracts.Requests;
+using Contracts.Responses;
+using Microsoft.AspNetCore.Mvc;
+using SmartWallet.Application.Services;
+using SmartWallet.Domain.Entities;
+using SmartWallet.Domain.Enums;
+
+
+namespace SmartWallet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WalletController : ControllerBase
+    {
+        private readonly IWalletService _service;
+
+        public WalletController(IWalletService service)
+        {
+            _service = service;
+        }
+
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Wallet>> GetById(Guid id)
+        {
+            var wallet = await _service.GetByIdAsync(id);
+            if (wallet == null) return NotFound();
+            return Ok(wallet);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<WalletResponse>> CreateAsync([FromBody] WalletRequest request)
+        {
+            if (!Enum.TryParse<CurrencyCode>(request.CurrencyCode, true, out var currency))
+                return BadRequest("Código de moneda inválido.");
+
+            var wallet = await _service.CreateAsync(
+                request.UserId,
+                request.Name,
+                currency,
+                request.Alias,
+                request.InitialBalance
+            );
+
+
+            var response = new WalletResponse
+            {
+                Id = wallet.Id,
+                UserId = wallet.UserID,
+                Name = wallet.Name,
+                CurrencyCode = currency.ToString(),
+                Alias = wallet.Alias,
+                Balance = wallet.Balance,
+                CreatedAt = wallet.CreatedAt
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = wallet.Id }, response);
+        }
+
+    }
+
+    
+}
--- src/SmartWallet.API/Program.cs
+++ src/SmartWallet.API/Program.cs
@@ -1,5 +1,8 @@
 using DotNetEnv;
+using SmartWallet.Application.Abstractions.Persistence;
+using SmartWallet.Application.Services;
 using SmartWallet.Infrastructure.Extensions;
+using SmartWallet.Infrastructure.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +24,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerDocumentation();
 builder.Services.AddSmartWalletInfrastructure(builder.Configuration);
+
+
+builder.Services.AddScoped<IWalletRepository, WalletRepository>();
+builder.Services.AddScoped<WalletService>();
+builder.Services.AddScoped<IWalletService, WalletService>();
+
 
 var app = builder.Build();
 
