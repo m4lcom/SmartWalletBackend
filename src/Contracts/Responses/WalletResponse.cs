db7871f3ba1ab117f39b490cf374245e77f5cc42
Ezequiel Natale <40499263+Eze-Nat@users.noreply.github.com>
2025-11-08 20:33:17 -0300

    agregado getall, get por alias, get por usuario

--- src/Contracts/Responses/WalletResponse.cs
+++ src/Contracts/Responses/WalletResponse.cs
@@ -4,8 +4,9 @@
     {
         public Guid Id  { get; set; }
         public Guid UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
-
         public string CurrencyCode { get; set; } = string.Empty;
         public string Alias { get; set; } = string.Empty;
         public decimal Balance { get; set; }
