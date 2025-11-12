8288a0026c6442a1f1fbfbb9e21cde14c73109ae
Julian Bison <80854425+JulianBison@users.noreply.github.com>
2025-11-12 16:09:22 -0300

    Refactor/user responses (#44)
    * UserResponses mejorados
    
    * Actualizacion de Docs de user-management

---
+++ src/Contracts/Responses/UserwithWalletResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Contracts.Responses
+{
+    public class UserwithWalletResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public UserRole Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool Active { get; set; }
+        public Guid WalletId { get; set; }
+        public string WalletAlias { get; set; } = string.Empty;
+    }
+}
